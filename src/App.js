e3ec6855c4621932f62a4c7aeb4edf3cc99ce525
jimmy doussain <doxteur>
2023-01-06 10:23:11 +0100

    :tada: Création du projet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+
+  const [monstate, setMonstate] = useState(0);
+
+  useEffect(() => {
+    console.log("monstate", monstate);
+  }, [monstate]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save toe reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => setMonstate(monstate + 1)}>Click me</button>
+        <p>{monstate}</p>
+        
       </header>
     </div>
   );
