eaaa45ccbacfd20f13125164bad9028cfc514e22
jimmy doussain <doxteur>
2023-01-26 10:09:17 +0100

    :bug: Bug Fix

--- src/App.js
+++ src/App.js
@@ -9,13 +9,17 @@ function App() {
 
   // fetch open meteo
   const [weather, setWeather] = useState(null);
+  const [ville, setVille] = useState({
+    name: "Paris",
+    lat: 48.8534,
+    lon: 2.3488,
+  });
 
   return (
     <div className="h-screen bg-[#1B1D1F]">
       <FormComponents weather={weather} setWeather={setWeather} />
-
       <Header weather={weather} setWeather={setWeather} />
-      <MeteoComponents weather={weather} setWeather={setWeather} />
+      <MeteoComponents weather={weather} setWeather={setWeather} ville={ville} setVille={setVille} />
     </div>
   );
 }
--- src/components/MeteoComponents.jsx
+++ src/components/MeteoComponents.jsx
@@ -4,33 +4,36 @@ import InfoMeteoComponent from "./InfoMeteoComponent";
 import MeteoByDayComponent from "./MeteoByDayComponent";
 import map from "../assets/Images/map.png";
 
-function MeteoComponents({ weather, setWeather }) {
+function MeteoComponents({ weather, setWeather, ville, setVille }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+
   useEffect(() => {
-    //fetch https://api.open-meteo.com/
+    //fetch https://api.open-meteo.com/ with lat and lon
     fetch(
-      "https://api.open-meteo.com/v1/forecast?latitude=48.8534&longitude=2.3488&hourly=temperature_2m"
+      `https://api.open-meteo.com/v1/forecast?latitude=${ville.lat}&longitude=${ville.lon}&hourly=temperature_2m`
     )
       .then((response) => {
         if (response.ok) {
           return response.json();
         } else {
-          throw response;
+          throw new Error("Something went wrong");
         }
       })
       .then((data) => {
-        console.log(data);
         setWeather(data);
+        console.log(data);
         setLoading(false);
+
       })
       .catch((error) => {
-        console.error(error);
         setError(error);
+        setLoading(false);
       });
-  }, []);
+  }, [setWeather, ville.lat, ville.lon]);
 
+  
   const weatherInfo = loading ? (
     <p>Loading...</p>
   ) : error ? (
